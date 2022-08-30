018dc5656dea4a173af58adeed9d36db2f52553e
Furkan Yıldırım <41998122+furknyldrm@users.noreply.github.com>
2022-08-30 23:38:32 +0300

    Errors are solved

--- src/router/index.js
+++ src/router/index.js
@@ -16,14 +16,6 @@ const router = new VueRouter({
       component: () => import('@/views/Home.vue'),
     },
     {
-      path: '/error-404',
-      name: 'error-404',
-      component: () => import('@/views/error/Error404.vue'),
-      meta: {
-        layout: 'full',
-      },
-    },
-    {
       path: '*',
       redirect: 'error-404',
     },
